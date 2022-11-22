a7064eb24ef8a5a52a5011d31bc42be25141c857
Fatou <fatoumbacke.diallo@gmail.com>
2022-11-22 22:09:59 +0100

    Collapsible component - About page on final stages

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -7,7 +7,7 @@ function about() {
     <section>
       <div className="aboutBanner"></div>
       <div>
-        <Collapsible title="Fiabilité">
+        <Collapsible label="Fiabilité">
           <p>
             Les annonces postées sur Kasa garantissent une fiabilité totale. Les
             photos sont conformes aux logements, et toutes les informations sont
@@ -16,7 +16,7 @@ function about() {
         </Collapsible>
       </div>
       <div>
-        <Collapsible title="Respect">
+        <Collapsible label="Respect">
           <p>
             La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
             comportement discriminatoire ou de perturbation du voisinage
@@ -25,7 +25,7 @@ function about() {
         </Collapsible>
       </div>
       <div>
-        <Collapsible title="Service">
+        <Collapsible label="Service">
           <p>
             Nos équipes se tiennent à votre disposition pour vous fournir une
             expérience parfaite. N'hésitez pas à nous contacter si vous avez la
@@ -34,7 +34,7 @@ function about() {
         </Collapsible>
       </div>
       <div>
-        <Collapsible title="Responsabilité">
+        <Collapsible label="Responsabilité">
           <p>
             La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
             pour les voyageurs, chaque logement correspond aux critères de
--- src/components/collapse.jsx
+++ src/components/collapse.jsx
@@ -1,36 +1,31 @@
 import React from "react";
-import Proptypes from "prop-types";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "../style/Collapse.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  FaCircleChevronDown,
-  FaCircleChevronUp,
-} from "@fortawesome/free-solid-svg-icons";
 
 function Collapsible(props) {
-  const [open, setOpen] = useState(false);
-  const { title, children } = this.props;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const parentRef = useRef();
+  if (parentRef.current) console.log();
 
   return (
-    <div className="Box">
-      <button
-        onClick={() => setOpen(!open)}
-        aria-controls={children}
-        aria-expanded={open}
-      ></button>
-      <div className="boxTitle">
-        <h2>{title}</h2>
-        <i className=" chevronStyle">
-          {FaCircleChevronDown || FaCircleChevronUp}
-        </i>
-      </div>
-      <div className="collapseBox">
-        <div className="boxChildren">{children}</div>
+    <div className="box">
+      <button className="buttonTitle" onClick={() => setIsOpen(!isOpen)}>
+        {props.label}
+      </button>
+      <div
+        className="boxParent"
+        ref={parentRef}
+        style={
+          isOpen
+            ? { height: parentRef.current.scrollHeight + "px" }
+            : { height: "0px" }
+        }
+      >
+        <div className="buttonChildren">{props.children}</div>
       </div>
     </div>
   );
 }
 
-Collapsible.propTypes = { title: Proptypes.string };
 export default Collapsible;
